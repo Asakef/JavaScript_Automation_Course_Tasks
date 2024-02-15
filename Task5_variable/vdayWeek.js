ccc6cf87e5f3fd53f4b878ab33ef167b3d556daf
Oluwafunmike Akinsanmi <fmsfunky@gmail.com>
2024-02-16 00:33:34 +0100

    TAsk5

---
+++ Task5_variable/vdayWeek.js
@@ -0,0 +1,12 @@
+
+
+const dayOfWeek='Thurday'
+
+const dayOfWeekPosition=4
+
+const dayOfWeekOutput1 ='Today is ' + dayOfWeek
+
+const dayOfWeekOutput2 ='It is' + ' ' + dayOfWeekPosition + 'th day of the week'
+
+console.log(dayOfWeekOutput1 + '   ' +  dayOfWeekOutput2 );
+ 
