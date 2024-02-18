2138164c8eb45e3113b2f03819ea9724a41f26d2
Oluwafunmike Akinsanmi <fmsfunky@gmail.com>
2024-02-18 21:55:53 +0100

    Adding Even No Tasks

---
+++ Task6_Even_numbers/even.js
@@ -0,0 +1,25 @@
+
+
+let   NumberEv = 1;
+
+while ( NumberEv <=20 ) {
+ if (NumberEv % 2=== 0 )
+ {
+    console.log( NumberEv +  ' ' + ' Is an Even Number')
+ }
+
+ NumberEv= NumberEv + 1
+
+
+}
+
+
+
+
+
+
+  
+
+
+
+
