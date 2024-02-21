059e2725972cda7976ddae661a5c976136272add
Oluwafunmike Akinsanmi <fmsfunky@gmail.com>
2024-02-21 23:29:57 +0100

    my Tasks Update

---
+++ Task10_FunCalARea/funarea.js
@@ -0,0 +1,19 @@
+
+
+function calAreaTriangle(length, width)  {
+    const areaTriangle = length *  width;
+    
+
+    //below return function
+    return areaTriangle;  
+ }
+ 
+ const length = 11;
+ const width = 8;
+ 
+ const areaOfRectangle = calAreaTriangle (length, width);
+ 
+ //Print output
+ console.log("Area of the rectangle:", areaOfRectangle);
+
+
