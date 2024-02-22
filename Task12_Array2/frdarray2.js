4761cc178cb6a4449eaabb2288a433c2315dccea
Oluwafunmike Akinsanmi <fmsfunky@gmail.com>
2024-02-22 22:27:12 +0100

    my tasks update

---
+++ Task12_Array2/frdarray2.js
@@ -0,0 +1,11 @@
+
+const myFrdArray=['Oluwafumike','Akinsola', 'Aderonke','Motunrayo', 'Evidence'];
+
+
+const bestFriendArray= myFrdArray[4]
+
+console.log(bestFriendArray)
+
+console.log(bestFriendArray.length)
+
+console.log(myFrdArray.length)
