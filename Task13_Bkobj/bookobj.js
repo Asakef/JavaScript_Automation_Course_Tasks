7ce39d21f7980354469a321eeb2149da0365c91a
Oluwafunmike Akinsanmi <fmsfunky@gmail.com>
2024-02-26 06:11:48 +0100

    TaskUpdate

---
+++ Task13_Bkobj/bookobj.js
@@ -0,0 +1,10 @@
+const myBook = {
+ booktitle: "The Young Shall Grow",
+ bookDescr: "Blue with Black color",
+ numberOfPages: 45,
+ bookAuthor: "Chinue Achebe",
+ IsReadable: false
+}
+
+console.log(myBook)
+console.log(myBook.IsReadable)
